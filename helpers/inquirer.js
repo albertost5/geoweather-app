f431699f99426ee3faa369a7ba115d8a36cc415a
albertost5 <sancheztorresalberto@gmail.com>
2022-03-24 19:22:34 +0100

    Added menu using inquirer.

---
+++ helpers/inquirer.js
@@ -0,0 +1,53 @@
+require('colors');
+const inquirer = require('inquirer');
+
+const optionsMenu = ['Exit', 'Find a place', 'Records'];
+
+const questionsMenu = {
+    type: 'list',
+    name: 'option',
+    message: 'What you want to do? ',
+    choices: [
+        {
+            value: 1,
+            name: `${ '1.'.green } ${ optionsMenu[1] }`
+        },
+        {
+            value: 2,
+            name: `${ '2.'.green } ${ optionsMenu[2] }`
+        },
+        {
+            value: 3,
+            name: `${ '3.'.green } ${ optionsMenu[0] }`
+        }
+    ]
+}
+
+async function showMenu() {
+    console.clear();
+    console.log('=================='.green);
+    console.log(' Select one option');
+    console.log('=================='.green);
+
+    const { option } = await inquirer.prompt(questionsMenu);
+
+    return option;
+}
+
+
+
+async function pause() {
+    const { pause } = await inquirer.prompt([{
+        type: 'input',
+        name: 'pause',
+        message: `Press ${'ENTER'.green } to continue.` 
+    }])
+}
+
+
+
+
+module.exports = {
+    showMenu,
+    pause
+}
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const { showMenu, pause } = require('./helpers/inquirer')
+
+const main = async() => {
+    let optSelected;
+    
+    do {
+        optSelected = await showMenu();
+
+        console.log(optSelected);
+
+        if ( optSelected != 3 ) await pause();
+    } while ( optSelected != 3)
+    
+}
+
+
+main();
